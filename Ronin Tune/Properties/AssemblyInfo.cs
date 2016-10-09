fa40f7d1c986bc550ab8080b5a13bc3450dc775f
Taazuma <multivita@ok.de>
2016-10-09 20:11:00 +0200

    Update AssemblyInfo.cs

--- Ronin Tune/Properties/AssemblyInfo.cs
+++ Ronin Tune/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ace38cee-7b1f-47ce-aa83-a71fe2949919")]
-[assembly: AssemblyVersion("1.3.3.7")]
-[assembly: AssemblyFileVersion("1.3.3.7")]
+[assembly: AssemblyVersion("6.20")]
+[assembly: AssemblyFileVersion("6.20")]
