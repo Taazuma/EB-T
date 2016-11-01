def7d65f96a5ce2f335e9d083b0a69c0190d3e70
Taazuma <tkaira@web.de>
2016-11-01 17:41:41 +0100

    killsteal

---
+++ Karthus Stealer/Program.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Constants;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+using static Eclipse.SpellsManager;
+using static Eclipse.Menus;
+using EloBuddy.SDK.Menu;
+using Color = System.Drawing.Color;
+using Eclipse;
+
+namespace Eclipse
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+        public static AIHeroClient _player
+        {
+            get { return ObjectManager.Player; }
+
+        }
+        private static readonly AIHeroClient player = ObjectManager.Player;
+
+        private static AIHeroClient qTarget;
+
+        private static AIHeroClient wTarget;
+
+        private static AIHeroClient eTarget;
+        public class UnitData
+        {
+            public static string Name;
+
+            public static int StartTime;
+
+            public static void GetName(AIHeroClient unit)
+            {
+                Name = unit.BaseSkinName;
+            }
+
+            public static void GetStartTime(int time)
+            {
+                StartTime = time;
+            }
+        }
+        public static int qOff = 0, wOff = 0, eOff = 0, rOff = 0;
+        private static int[] AbilitySequence;
+        public static int start = 0;
+        private static int drawTick;
+        private static Sprite introImg;
+        private static bool check(Menu submenu, string sig)
+        {
+            return submenu[sig].Cast<CheckBox>().CurrentValue;
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            //Put the name of the champion here
+            Chat.Print("Have Fun with Playing ! by Taazuma Credits full to Kappa the Kapp");
+            Core.DelayAction(() =>
+            {
+                //introImg = new Sprite(TextureLoader.BitmapToTexture(Resources.anime));
+                Drawing.OnDraw += DrawingOnOnDraw;
+                Core.DelayAction(() =>
+                {
+                    Drawing.OnDraw -= DrawingOnOnDraw;
+                }, 7000);
+            }, 2000);
+            AbilitySequence = new int[] { 3, 2, 3, 1, 3, 4, 3, 2, 3, 2, 4, 2, 2, 1, 1, 4, 1, 1 };
+            SpellsManager.InitializeSpells();
+            Menus.CreateMenu();
+            Game.OnTick += GameOnTick;
+            Drawing.OnDraw += Drawing_OnDraw;
+            Obj_AI_Base.OnNewPath += Obj_AI_Base_OnNewPath;
+            Game.OnUpdate += OnUpdate;
+        }
+
+        private static void OnUpdate(EventArgs args)
+        {
+            if (player.IsDead)
+            {
+                return;
+            }
+
+            qTarget = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+            wTarget = TargetSelector.GetTarget(W.Range, DamageType.Magical);
+            eTarget = TargetSelector.GetTarget(E.Range, DamageType.Magical);
+
+            if (FirstMenu.Get<ComboBox>("UltMode").CurrentValue == 0 && FirstMenu.Get<CheckBox>("UltKS").CurrentValue && (R.IsLearned && R.IsReady()))
+            {
+                Ult();
+            }
+
+            if (FirstMenu.GetCheckBoxValue("KS"))
+            {
+                Ks();
+            }
+
+        }
+
+        #region Stealer
+
+        private static void Ks()
+        {
+            if (qTarget != null && KillStealMenu.Get<CheckBox>("KS").CurrentValue)
+            {
+                if (qTarget.TotalShieldHealth() < player.GetSpellDamage(qTarget, SpellSlot.Q))
+                {
+                    Q.Cast(qTarget.Position);
+                }
+            }
+        }
+
+        private static void Ult() // CREDITS ALL TO KAPPA THE KAPP KAPP
+        {
+            // Kappa ult logic.
+            var time = Environment.TickCount;
+            var enemiesrange = ObjectManager.Player.Position.CountEnemiesInRange(FirstMenu.Get<Slider>("Rranged").CurrentValue);
+            var enemieinsrange = FirstMenu.Get<Slider>("RnearEn").CurrentValue;
+            foreach (var rtarget in
+                EntityManager.Heroes.Enemies.Where(
+                    x =>
+                    x != null && x.IsValid && !x.IsDead && x.IsEnemy
+                    && (!x.HasBuff("kindrednodeathbuff") || !x.HasBuff("Undying Rage") || !x.HasBuff("JudicatorIntervention")) && !x.IsZombie
+                    && player.GetSpellDamage(x, SpellSlot.R) > Prediction.Health.GetPrediction(x, (int)(R.CastDelay * 1000f))
+                    && x.CountAlliesInRange(750) < 1))
+            {
+                if (FirstMenu.Get<CheckBox>("RnearE").CurrentValue && enemieinsrange <= enemiesrange)
+                {
+                    R.Cast(rtarget.Position);
+                }
+
+                if (!FirstMenu.Get<CheckBox>("RnearE").CurrentValue)
+                {
+                    R.Cast(rtarget.Position);
+                }
+
+                if (player.IsZombie)
+                {
+                    R.Cast(rtarget.Position);
+                }
+            }
+        }
+
+        #endregion Stealer
+
+        #region Image
+
+        private static void DrawingOnOnDraw(EventArgs args)
+        {
+            if (drawTick == 0)
+                drawTick = Environment.TickCount;
+
+            int timeElapsed = Environment.TickCount - drawTick;
+            introImg.CenterRef = new Vector2(Drawing.Width / 2f, Drawing.Height / 2f).To3D();
+
+            int dt = 300;
+            if (timeElapsed <= dt)
+                introImg.Scale = new Vector2(timeElapsed * 1f / dt, timeElapsed * 1f / dt);
+            introImg.Draw(new Vector2(Drawing.Width / 2f - 1415 / 2f, Drawing.Height / 2f - 750 / 2f));
+        }
+
+#endregion Image
+
+        #region Misc
+
+        private static void GameOnTick(EventArgs args)
+        {
+            if (check(MiscMenu, "skinhax")) _player.SetSkinId((int)MiscMenu["skinID"].Cast<ComboBox>().CurrentValue);
+            if (check(MiscMenu, "lvlup")) LevelUpSpells();
+        }
+
+        private static void LevelUpSpells() // Thanks iRaxe
+        {
+            var qL = _player.Spellbook.GetSpell(SpellSlot.Q).Level + qOff;
+            var wL = _player.Spellbook.GetSpell(SpellSlot.W).Level + wOff;
+            var eL = _player.Spellbook.GetSpell(SpellSlot.E).Level + eOff;
+            var rL = _player.Spellbook.GetSpell(SpellSlot.R).Level + rOff;
+            if (qL + wL + eL + rL >= ObjectManager.Player.Level) return;
+            var level = new[] { 0, 0, 0, 0 };
+            for (var i = 0; i < ObjectManager.Player.Level; i++)
+            {
+                level[AbilitySequence[i] - 1] = level[AbilitySequence[i] - 1] + 1;
+            }
+            if (qL < level[0]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.Q);
+            if (wL < level[1]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.W);
+            if (eL < level[2]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.E);
+            if (rL < level[3]) ObjectManager.Player.Spellbook.LevelSpell(SpellSlot.R);
+        }// Thanks Iraxe
+
+        #endregion Misc
+
+        #region Draws
+        // Draws
+
+        private static void Obj_AI_Base_OnNewPath(Obj_AI_Base sender, GameObjectNewPathEventArgs args)
+        {
+            if (!sender.IsMe)
+                return;
+
+            start = TickCount;
+        }
+
+        public static int TickCount
+        {
+            get
+            {
+                return Environment.TickCount & int.MaxValue;
+            }
+        }
+
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            if (!DrawingsMenu["toggle"].Cast<KeyBind>().CurrentValue || !Enable())
+                return;
+
+            var ETA = DrawingsMenu["eta"].Cast<CheckBox>().CurrentValue;
+            var Name = DrawingsMenu["name"].Cast<CheckBox>().CurrentValue;
+            var Thickness = DrawingsMenu["thick"].Cast<Slider>().CurrentValue;
+
+            foreach (var hero in EntityManager.Heroes.AllHeroes.Where(h => h.IsValid))
+            {
+                if (DrawingsMenu["me"].Cast<CheckBox>().CurrentValue && hero.IsMe)
+                {
+                    DrawPath(Player.Instance, Thickness, Color.LawnGreen);
+
+                    if (ETA && Player.Instance.Path.Length > 1 && Player.Instance.IsMoving)
+                        Drawing.DrawText(Player.Instance.Path[Player.Instance.Path.Length - 1].WorldToScreen(), Color.NavajoWhite, GetETA(Player.Instance), 10);
+
+                    continue;
+                }
+
+                if (DrawingsMenu["ally"].Cast<CheckBox>().CurrentValue && hero.IsAlly && !hero.IsMe)
+                {
+                    DrawPath(hero, Thickness, Color.Orange);
+
+                    if (hero.Path.Length > 1 && hero.IsMoving)
+                    {
+                        if (Name)
+                            Drawing.DrawText(hero.Path[hero.Path.Length - 1].WorldToScreen(), Color.LightSkyBlue, hero.BaseSkinName, 10);
+
+                        if (ETA && false)
+                            Drawing.DrawText(hero.Path[hero.Path.Length - 1].WorldToScreen() + new Vector2(0, 20), Color.NavajoWhite, GetETA(hero), 10);
+                    }
+
+                    continue;
+                }
+
+                if (DrawingsMenu["enemy"].Cast<CheckBox>().CurrentValue && hero.IsEnemy)
+                {
+                    DrawPath(hero, Thickness, Color.Red);
+
+                    if (hero.Path.Length > 1 && hero.IsMoving)
+                    {
+                        if (Name)
+                            Drawing.DrawText(hero.Path[hero.Path.Length - 1].WorldToScreen(), Color.LightSkyBlue, hero.BaseSkinName, 10);
+
+                        if (ETA && false)
+                            Drawing.DrawText(hero.Path[hero.Path.Length - 1].WorldToScreen() + new Vector2(0, 20), Color.NavajoWhite, GetETA(hero), 10);
+                    }
+
+                    continue;
+                }
+            }
+        }
+
+        public static void DrawPath(AIHeroClient unit, int thickness, Color color)
+        {
+            if (!unit.IsMoving)
+                return;
+
+            for (var i = 1; unit.Path.Length > i; i++)
+            {
+                if (unit.Path[i - 1].IsValid() && unit.Path[i].IsValid() && (unit.Path[i - 1].IsOnScreen() || unit.Path[i].IsOnScreen()))
+                {
+                    Drawing.DrawLine(Drawing.WorldToScreen(unit.Path[i - 1]), Drawing.WorldToScreen(unit.Path[i]), thickness, color);
+                }
+            }
+        }
+
+        public static string GetETA(AIHeroClient unit)
+        {
+            float Distance = 0;
+
+            if (unit.Path.Length > 1)
+            {
+                for (var i = 1; unit.Path.Length > i; i++)
+                {
+                    Distance += unit.Path[i - 1].Distance(unit.Path[i]);
+                }
+            }
+
+            var ETA = (start + Distance / unit.MoveSpeed * 1000 - TickCount) / 1000;
+
+            if (ETA <= 0)
+                ETA = 0;
+
+            return ETA.ToString("F2");
+        }
+
+        public static bool Enable()
+        {
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Combo && DrawingsMenu["combo"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Harass && DrawingsMenu["harass"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if ((Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LaneClear || Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.JungleClear) && DrawingsMenu["laneclear"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.LastHit && DrawingsMenu["lasthit"].Cast<CheckBox>().CurrentValue)
+                return false;
+            if (Orbwalker.ActiveModesFlags == Orbwalker.ActiveModes.Flee && DrawingsMenu["flee"].Cast<CheckBox>().CurrentValue)
+                return false;
+            return true;
+        }
+        #endregion Draws
+
+    }
+}
