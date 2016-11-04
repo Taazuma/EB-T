ebc12e0aa425379576f45914acfdd9bfaee0c3b3
Taazuma <tkaira@web.de>
2016-11-04 14:20:48 +0100

    123

--- Ronin Skarner/Properties/AssemblyInfo.cs
+++ Ronin Skarner/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ace38cee-7b1f-47ce-aa83-a71fe2949919")]
-[assembly: AssemblyVersion("6.20")]
-[assembly: AssemblyFileVersion("6.20")]
+[assembly: AssemblyVersion("6.21")]
+[assembly: AssemblyFileVersion("6.21")]
